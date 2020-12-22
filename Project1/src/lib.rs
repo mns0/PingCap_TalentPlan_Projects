70b04d98d3d1e47d0ce2862a3a13efe71a013b92
Manish Shankla <manish@Manishs-MacBook-Pro-2.local>
2020-12-21 19:26:57 -0800

    first commit

---
+++ Project1/src/lib.rs
@@ -0,0 +1,19 @@
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        unimplemented!()
+    }
+
+    pub fn set(&self, key: String, value: String) {
+        unimplemented!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        unimplemented!()
+    }
+
+    pub fn remove(&self, key: String) -> Option<String> {
+        unimplemented!()
+    }
+}
