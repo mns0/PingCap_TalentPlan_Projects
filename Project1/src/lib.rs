51813c0ac9e1d5024200be30f8f6b69504200c7f
Manish Shankla <manish@Manishs-MacBook-Pro-2.local>
2020-12-21 20:13:47 -0800

    first commit

--- Project1/src/lib.rs
+++ Project1/src/lib.rs
@@ -1,19 +1,25 @@
-pub struct KvStore;
+use std::collections::HashMap;
+
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> KvStore {
-        unimplemented!()
+        KvStore {
+            map: HashMap::new(),
+        }
     }
 
-    pub fn set(&self, key: String, value: String) {
-        unimplemented!()
+    pub fn set(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        unimplemented!()
+        self.map.get(&key).cloned()
     }
 
-    pub fn remove(&self, key: String) -> Option<String> {
-        unimplemented!()
+    pub fn remove(&mut self, key: String) -> Option<String> {
+        self.map.remove(&key)
     }
 }
